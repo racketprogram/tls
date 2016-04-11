3ee32e02cecf2b21fd860bdacd20ff91165af255
rainbow <racketprogram@gmail.com>
2016-04-11 14:49:09 +0800

    subset

--- ch1.scm
+++ ch1.scm
@@ -14,3 +14,17 @@
     (cond
      ((null? l) '())
      (else (cons (car (car l)) (first (cdr l)))))))
+
+(define insert
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     ((eq? old (car lat)) (cons (car lat) (cons new (insert new old (cdr lat)))))
+     (else (cons (car lat) (insert new old (cdr lat)))))))
+(define subset
+   (lambda (n o1 o2 lat)
+    (cond
+     ((null? lat) '())
+     ((eq? o1 (car lat)) (cons n (cdr lat)))
+     ((eq? o2 (car lat)) (cons n (cdr lat)))
+     (else (cons (car lat) (subset n o1 o2 (cdr lat)))))))
