9c264274a7500d43813fe0ecb820ff6fc169bdee
rainbow <racketprogram@gmail.com>
2016-04-12 15:20:47 +0800

    + ch4.scm

--- ch4.scm
+++ ch4.scm
@@ -18,3 +18,94 @@
      ((zero? m) n)
      (else (o- (sub1 n) (sub1 m))))))
 
+(define addtup
+  (lambda (tup)
+    (cond
+     ((null? tup) 0)
+     (else (o+ (car tup) (addtup (cdr tup)))))))
+
+(define o*
+  (lambda (n m)
+    (cond
+     ((zero? m) 0)
+     (else  (o+ n (o* n (sub1 m)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+     ((null? tup1) tup2)
+     ((null? tup2) tup1)
+     (else (cons (o+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define o>
+  (lambda (n m)
+    (cond
+     ((zero? n) #f)
+     ((zero? m) #t)
+     (else (o> (sub1 n) (sub1 m))))))
+
+(define o<
+  (lambda (n m)
+    (cond
+     ((zero? m) #f)
+     ((zero? n) #t)
+     (else (o< (sub1 n) (sub1 m))))))
+
+(define o=
+  (lambda (n m)
+    (cond
+     ((or (o> n m) (o< n m)) #f)
+     (else #t))))
+
+(define o^
+  (lambda (n m)
+    (cond
+     ((zero? m) 1)
+     (else (o* n (o^ n (sub1 m)))))))
+
+(define o/
+  (lambda (n m)
+    (cond
+     ((< n m) 0)
+     (else (add1 (o/ (o- n m) m))))))
+
+(define length
+  (lambda (lat)
+    (cond
+     ((null? lat) 0)
+     (else (add1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+     ((null? lat) 'error)
+     ((zero? (sub1 n)) (car lat))
+     (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+     ((null? lat) '())
+     ((zero? (sub1 n)) (cdr lat))
+     (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+     ((null? lat) '())
+     ((number? (car lat)) (no-nums (cdr lat)))
+     (else (cons (car lat) (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+     ((null? lat) '())
+     ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+     (else (all-nums (cdr lat))))))
+
+(define epan?
+  (lambda (n m)
+    (cond
+     ((and (number? n) (number? m)) (o= n m))
+     ((or (number? n) (number? m)) #f)
+     (else (eq? n m))))) 
