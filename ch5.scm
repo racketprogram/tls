3f889f63cb636e98ccea0da1df4fc1a11dc2e421
rainbow <racketprogram@gmail.com>
2016-04-12 19:36:58 +0800

    + ch5.scm

--- ch5.scm
+++ ch5.scm
@@ -2,10 +2,19 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
-(define rember*
+(define rember**
   (lambda (a l)
     (cond
      ((null? l) '())
      ((atom? l) l)
-     ((eq? a (car l)) (rember* a (cdr l)))
+     ((eq? a (car l)) (rember** a (cdr l)))
+     (else (cons (rember** a (car l)) (rember** a (cdr l)))))))
+
+(define rember*
+  (lambda (a l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l)) (cond
+                       ((eq? (car l) a) (rember* a (cdr l)))
+                       (else (cons (car l) (rember* a (cdr l))))))
      (else (cons (rember* a (car l)) (rember* a (cdr l)))))))
