e95baee80b4b5c67061647bb8718290e4a7d12a0
rainbow <racketprogram@gmail.com>
2016-04-12 20:55:40 +0800

    + ch5.scm

--- ch5.scm
+++ ch5.scm
@@ -18,3 +18,30 @@
                        ((eq? (car l) a) (rember* a (cdr l)))
                        (else (cons (car l) (rember* a (cdr l))))))
      (else (cons (rember* a (car l)) (rember* a (cdr l)))))))
+
+(define insertR*
+  (lambda (n o l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l)) (cond
+                       ((eq? (car l) o) (cons o (cons n (insertR* n o (cdr l)))))
+                       (else (cons (car l) (insertR* n o (cdr l))))))
+     (else (cons (insertR* n o (car l)) (insertR* n o (cdr l)))))))
+
+(define occur*
+  (lambda (a l)
+    (cond
+     ((null? l) 0)
+     ((atom? (car l)) (cond
+                       ((eq? (car l) a) (add1 (occur* a (cdr l))))
+                       (else (occur* a (cdr l)))))
+     (else (o+ (occur* a (car l)) (occur* a (cdr l)))))))
+
+(define subset*
+  (lambda (n o l)
+    (cond
+     ((null? l) '())
+     ((atom? (car l)) (cond
+                       ((eq? (car l) o) (cons n (subset* n o (cdr l))))
+                       (else (cons (car l) (subset* n o (cdr l))))))
+     (else (cons (subset* n o (car l)) (subset* n o (cdr l)))))))
