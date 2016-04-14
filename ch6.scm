eb1126695bff1429b5e0dac12218660b20f18295
rainbow <racketprogram@gmail.com>
2016-04-14 15:49:22 +0800

    finish ch6.scm

--- ch6.scm
+++ ch6.scm
@@ -35,3 +35,67 @@
      ((eq? (car (cdr nexp)) 'o^)
       (o^ (value (car nexp))
           (value (car (cdr (cdr nexp)))))))))
+(define value2
+  (lambda (nexp)
+    (cond
+     ((atom? nexp) nexp)
+     ((eq? (car nexp) 'o+)
+      (o+ (value2 (car (cdr nexp)))
+          (value2 (car (cdr (cdr nexp))))))
+     ((eq? (car nexp) 'o-)
+      (o- (value2 (car (cdr nexp)))
+          (value2 (car (cdr (cdr nexp))))))
+     ((eq? (car nexp) 'o*)
+      (o* (value2 (car (cdr nexp)))
+          (value2 (car (cdr (cdr nexp)))))))))
+
+(define operator
+  (lambda (aexp)
+    (car aexp)))
+
+(define 1st-sub-exp
+  (lambda (aexp)
+    (car (cdr aexp))))
+
+(define 2nd-sub-exp
+  (lambda (aexp)
+    (car (cdr (cdr aexp)))))
+
+(define value3
+  (lambda (nexp)
+    (cond
+     ((atom? nexp) nexp)
+     ((eq? (operator nexp) 'o+)
+      (o+ (value3 (1st-sub-exp nexp))
+          (value3 (2nd-sub-exp nexp))))
+     ((eq? (operator nexp) 'o-)
+      (o- (value3 (1st-sub-exp nexp))
+          (value3 (2nd-sub-exp nexp))))
+     ((eq? (operator nexp) 'o*)
+      (o/ (value3 (1st-sub-exp nexp))
+          (value3 (2nd-sub-exp nexp))))
+     ((eq? (operator nexp) 'o/)
+      (o/ (value3 (1st-sub-exp nexp))
+          (value3 (2nd-sub-exp nexp))))
+     ((eq? (operator nexp) 'o^)
+      (o^ (value3 (1st-sub-exp nexp))
+          (value3 (2nd-sub-exp nexp)))))))
+
+(define sero?
+  (lambda (n)
+    (null? n)))
+
+(define edd1
+  (lambda (n)
+    (cons '() n)))
+
+(define zub1
+  (lambda (n)
+    (cdr n)))
+
+(define oo+
+  (lambda (n m)
+    (cond
+     ((null? n) m)
+     (else (edd1 (oo+ (zub1 n) m))))))
+
