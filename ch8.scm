8ff12b42b55bcb5818cec07db65c68360823aee2
rainbow <racketprogram@gmail.com>
2016-04-17 05:27:31 +0800

    + ch8.scm

--- ch8.scm
+++ ch8.scm
@@ -4,3 +4,27 @@
      ((null? l) '())
      ((test? a (car l)) (cdr l))
      (else (cons (car l) (rember-f test? a (cdr l)))))))
+
+(define eq?-c
+  (lambda (a)
+    (lambda (x)
+      (eq? a x ))))
+
+(define eq?-salad (eq?-c 'salad))
+
+(define rember-f
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+       ((null? l) '())
+       ((test? a (car l)) (cdr l))
+       (else (cons (car l) ((rember-f test?) a (cdr l))))))))
+
+(define insertL-f
+  (lambda (test?)
+    (lambda (n o l)
+      (cond
+       ((null? l) '())
+       ((test? o (car l))
+        (cons n (cons o ((insertL-f test?) n o (cdr l)))))
+       (else (cons (car l) ((insertL-f test?) n o (cdr l))))))))
